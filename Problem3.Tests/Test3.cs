95dcc86cd8a2f53280dd66dce4ec51a4cec545de
MaxWhitehead1999 <157076920+MaxWhitehead1999@users.noreply.github.com>
2026-04-07 20:48:02 -0400

    Add files via upload

---
+++ Problem3.Tests/Test3.cs
@@ -0,0 +1,11 @@
+﻿namespace Problem3.Tests
+{
+    [TestClass]
+    public sealed class Test3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Problem4.Tests/Test4.cs
@@ -0,0 +1,11 @@
+﻿namespace Problem4.Tests
+{
+    [TestClass]
+    public sealed class Test4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
